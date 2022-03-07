d9f993c927332b9abb99428e41a7e6e6bde71203
Ákos Németh <nemeth.akos@gmail.com>
2022-03-07 21:58:38 +0100

    Added gradle build

--- src/main/java/LotteryTree.java
+++ src/main/java/LotteryTree.java
@@ -20,15 +20,14 @@ public class LotteryTree {
 	 */
 	public void addNumbers(int... numbers) {
 
-		validateNumbers(5, numbers);
+		validateLotteryNumbersForCountAndNonRepetition(5, numbers);
 
 		LotteryTreeNode node = root;
-		for (int i = 0; i < numbers.length; i++) {
+		for (var number: numbers) {
 			node.increaseCount();
-			node = node.getChild(numbers[i]);
+			node = node.getChild(number);
 		}
 		node.increaseCount();
-
 	}
 
 	/**
@@ -37,28 +36,33 @@ public class LotteryTree {
 	 * @param numbers the drawn numbers
 	 * @throws IllegalArgumentException if the numbers are not valid lottery numbers
 	 */
-	public void printWinners(int... numbers) {
-		validateNumbers(5, numbers);
+	public String getWinnersText(int... numbers) {
+		validateLotteryNumbersForCountAndNonRepetition(5, numbers);
+		StringBuilder sb = new StringBuilder();
+
 		for (var i = 5; i >= 2; i--) {
 			List<Set<Integer>> combinations = getAllCombinations(i, numbers);
 			var count = 0;
-			for (Set<Integer> c : combinations) {
-				int[] combinationNumbers = new int[c.size()];
-				var ci = c.iterator();
+			for (Set<Integer> combination : combinations) {
+				int[] combinationNumbers = new int[combination.size()];
+				var combinationIterator = combination.iterator();
 				var index = 0;
-				while (ci.hasNext()) {
-					combinationNumbers[index++] = ci.next();
+				while (combinationIterator.hasNext()) {
+					combinationNumbers[index++] = combinationIterator.next();
 				}
-				int combinationCount = queryNumbers(combinationNumbers);
-				System.out.format("\t%s: %d\n", Arrays.toString(combinationNumbers), combinationCount);
+				int combinationCount = getCombinationCount(combinationNumbers);
+				if (combinationCount > 0) {
+					sb.append(String.format("\t%s: %d\n",
+											Arrays.toString(combinationNumbers), combinationCount));
+				}
 				count += combinationCount;
 			}
-			System.out.format("#%d: %d\n", i, count);
+			sb.append(String.format("#%d: %d\n", i, count));
 		}
-
+		return sb.toString();
 	}
 
-	protected List<Set<Integer>> getAllCombinations(int count, int... numbers) {
+	 List<Set<Integer>> getAllCombinations(int count, int... numbers) {
 		List<Set<Integer>> accumulator = new ArrayList<>();
 		boolean[] used = new boolean[numbers.length];
 
@@ -67,11 +71,11 @@ public class LotteryTree {
 		return accumulator;
 	}
 
-	protected int queryNumbers(int... numbers) {
+	int getCombinationCount(int... numbers) {
 		if (numbers.length < 1 || numbers.length > 5) {
 			throw new IllegalArgumentException("Illegal count of numbers: " + numbers.length);
 		}
-		validateNumbers(numbers.length, numbers);
+		validateLotteryNumbersForCountAndNonRepetition(numbers.length, numbers);
 
 		LotteryTreeNode[] nodes = new LotteryTreeNode[numbers.length];
 		LotteryTreeNode node = root;
@@ -80,52 +84,46 @@ public class LotteryTree {
 		for (int i = 0; i < numbers.length; i++) {
 			node = node.getChild(numbers[i]);
 			count = node.getCount();
-			System.out.println("\t\t\t\t(" + (i + 1) + "): " + numbers[i] + ", c:" + count);
+			//System.out.println("\t\t\t\t(" + (i + 1) + "): " + numbers[i] + ", c:" + count);
 			nodes[i] = node;
 		}
 
-		for (int i = 0; i < nodes.length; i++) {
-			nodes[i].decreaseCount(count);
+		for (var nodeToDecrease: nodes) {
+			nodeToDecrease.decreaseCount(count);
 		}
 
 		return count;
 	}
 
-	protected void subset(int[] A, int k, int start, int currLen, boolean[] used, List<Set<Integer>> accumulator) {
+	 private void subset(int[] A, int k, int start, int currLen, boolean[] used, List<Set<Integer>> accumulator) {
 
 		Set<Integer> set = new HashSet<>();
 		if (currLen == k) {
 			for (int i = 0; i < A.length; i++) {
-				if (used[i] == true) {
+				if (used[i]) {
 					set.add(A[i]);
-					// System.out.print(A[i] + " ");
 				}
 			}
 			accumulator.add(set);
-			// System.out.println();
 			return;
 		}
 		if (start == A.length) {
 			return;
 		}
-		// For every index we have two options,
-		// 1.. Either we select it, means put true in used[] and make currLen+1
 		used[start] = true;
 		subset(A, k, start + 1, currLen + 1, used, accumulator);
-		// 2.. OR we dont select it, means put false in used[] and dont increase
-		// currLen
 		used[start] = false;
 		subset(A, k, start + 1, currLen, used, accumulator);
 	}
 
-	protected void validateNumbers(int requiredCount, int... numbers) {
+	private void validateLotteryNumbersForCountAndNonRepetition(int requiredCount, int... numbers) {
 		if (numbers.length != requiredCount) {
 			throw new IllegalArgumentException("Five numbers required" + Arrays.toString(numbers));
 		}
 
-		for (int i = 0; i < numbers.length; i++) {
-			if (numbers[i] < 1 || numbers[i] > 90) {
-				throw new IllegalArgumentException("Invalid number" + numbers[i]);
+		for (var number: numbers) {
+			if (number < 1 || number > 90) {
+				throw new IllegalArgumentException("Invalid number: " + number);
 			}
 		}
 
---
+++ src/test/java/LotteryTreeTest.java
@@ -0,0 +1,117 @@
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LotteryTreeTest {
+
+    public static final int[] NUMBERS = new int[]{1, 2, 3, 4, 5};
+
+    @Test
+    void testAllSubSets() {
+        LotteryTree lotteryTree = new LotteryTree();
+
+        var expectedResult = new ArrayList<Set<Integer>>(
+                Collections.singleton(
+                        new HashSet<>(
+                                Arrays.stream(NUMBERS)
+                                        .boxed()
+                                        .collect(Collectors.toList())
+                        )));
+
+        assertEquals(
+                lotteryTree.getAllCombinations(5, NUMBERS),
+                expectedResult);
+    }
+
+    @Disabled
+    @Test
+    void testRandom() {
+        LotteryTree lt = new LotteryTree();
+
+        Random r = new Random();
+        int[] numbers = new int[5];
+        int i = 1;
+        while (i <= 10_000_000) {
+            if (i % 1_000_000 == 0) {
+                System.out.println(i);
+            }
+            getRandomNumbers(r, numbers);
+            try {
+                lt.addNumbers(numbers);
+                i++;
+            } catch (IllegalArgumentException e) {
+                // System.out.println("Wrong numbers:" + Arrays.toString(numbers));
+            }
+
+        }
+
+        i = 0;
+        while (i == 0) {
+            getRandomNumbers(r, numbers);
+            lt.getWinnersText(numbers);
+            i += 1;
+
+        }
+    }
+
+    private void getRandomNumbers(Random r, int[] numbers) {
+        for (int j = 0; j < 5; j++) {
+            numbers[j] = r.nextInt(89) + 1;
+        }
+    }
+
+    @Disabled
+    @Test
+    void testAddNumbers() {
+
+        LotteryTree lt = new LotteryTree();
+
+        for (int n1 = 1; n1 < 90 - 4; n1++) {
+
+            for (int n2 = n1 + 1; n2 < 90 - 3; n2++) {
+
+                for (int n3 = n2 + 1; n3 < 90 - 2; n3++) {
+
+                    for (int n4 = n3 + 1; n4 < 90 - 1; n4++) {
+
+                        int n5 = n4 + 1;
+
+                        lt.addNumbers(n1, n2, n3, n4, n5);
+
+                    }
+                }
+            }
+        }
+
+        lt.getWinnersText(86, 87, 88, 89, 90);
+    }
+
+    @Test
+    void testPrintWinners() {
+
+        LotteryTree lt = new LotteryTree();
+
+        lt.addNumbers(1, 2, 3, 4, 5);
+        lt.addNumbers(1, 2, 3, 4, 10);
+        lt.addNumbers(1, 2, 3, 9, 10);
+        lt.addNumbers(1, 2, 8, 9, 10);
+        lt.addNumbers(1, 9, 10, 11, 12);
+
+        var s = lt.getWinnersText(1, 2, 3, 4, 5);
+        assertEquals("""
+                             	[1, 2, 3, 4, 5]: 1
+                             #5: 1
+                             	[1, 2, 3, 4]: 1
+                             #4: 1
+                             	[1, 2, 3]: 1
+                             #3: 1
+                             	[1, 2]: 1
+                             #2: 1
+                             """, s);
+    }
+
+}
